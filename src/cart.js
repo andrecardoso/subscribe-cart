bb768ce3c184aaceb5098745aeb5203b2c4e7fd0
André Luiz Cardoso <andrecardoso@gmail.com>
2020-07-24 18:40:21 -0300

    Move item grouping to Cart class

--- src/cart.js
+++ src/cart.js
@@ -1,18 +1,29 @@
 export default class Cart {
   constructor(data) {
-    this.products = data.products || []
+    this._products = data.products || []
   }
 
   get numberOfProducts() {
-    return this.products.length
+    return this._products.length
   }
 
   taxes() {
-    return this.products.reduce((total, p) => total + p.taxes(), 0)
+    return this._products.reduce((total, p) => total + p.taxes(), 0)
   }
 
   total() {
-    const totalPrice = this.products.reduce((total, p) => total + p.price, 0)
+    const totalPrice = this._products.reduce((total, p) => total + p.price, 0)
     return totalPrice + this.taxes()
+  }
+
+  get items() {
+    return this._products.reduce((result, p) => {
+      const item = result[p.name]
+      if (!item) {
+        return { [p.name]: { quantity: 1, price: p.price.toFixed(2) } }
+      }
+      item.quantity = item.quantity + 1
+      return result
+    }, {})
   }
 }
--- src/receipt-renderer.js
+++ src/receipt-renderer.js
@@ -1,13 +1,4 @@
-function renderProducts(products) {
-  const items = products.reduce((result, p) => {
-    const item = result[p.name]
-    if (!item) {
-      return { [p.name]: { quantity: 1, price: p.price.toFixed(2) } }
-    }
-    item.quantity = item.quantity + 1
-    return result
-  }, {})
-
+function renderItems(items) {
   return Object.entries(items).reduce(
     (result, [name, item]) =>
       `${result ? '\n' : ''}${item.quantity} ${name}: ${
@@ -20,7 +11,7 @@ function renderProducts(products) {
 function renderReceipt(cart) {
   const taxes = `Sales Taxes: ${cart.taxes().toFixed(2)}`
   const total = `Total: ${cart.total().toFixed(2)}`
-  return `${renderProducts(cart.products)}\n${taxes}\n${total}`
+  return `${renderItems(cart.items)}\n${taxes}\n${total}`
 }
 
 export { renderReceipt }
