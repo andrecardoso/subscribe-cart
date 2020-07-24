3d8cc3f80efcab6d2eea87d95f90bb6d12613d46
André Luiz Cardoso <andrecardoso@gmail.com>
2020-07-24 15:50:07 -0300

    Add initial structure
    - Node
    - Babel for modern javascript
    - Mocha for testing
    - http-server for local development
    - editorconfig, eslint and prettier for code formatting standard
    - initial UI to input cart description as in the spec

---
+++ index.js
@@ -0,0 +1,12 @@
+import Product from '../src/product.js'
+
+export default function calculate(input) {
+  console.log(input)
+  return `
+  2 book: 24.98
+  1 music CD: 16.49
+  1 chocolate bar: 0.85
+  Sales Taxes: 1.50
+  Total: 42.32
+  `
+}
